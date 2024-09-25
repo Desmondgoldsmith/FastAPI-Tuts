9ababeb0fb26a726b415c913582cf0ab6c84e877
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-09-25 20:49:23 +0000

    feat: Modifying the code to make use of SQLAlchemy ORM

--- app/database.py
+++ app/database.py
@@ -5,3 +5,7 @@ from sqlalchemy.orm import sessionmaker
 SQLALCHEMY_DATABASE_URL = 'postgres://postgres:DessyAdmin@localhost/FASTAPI_DB'
 # establishes the postgres conn 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
