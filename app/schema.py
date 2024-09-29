1f4b442f2731a6c6de8c420a56e4d50a0ec97628
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-09-29 00:54:11 +0000

    chore/ writing out the schemas

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class validatePosts(BaseModel):
     title: str
     content: str
     published: bool
+    
+    
+class Post(BaseModel):
+    id: int
+    title: str
+    content: str
+    published: bool
+    created_at: datetime 
+    
+    class config:
+        orm_model = True
