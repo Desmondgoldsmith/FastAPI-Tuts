eab2000dce848cc05ca899e81f4b534edf6c0e96
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-07 14:03:13 +0000

    chore/ added a check to validate if user exist

--- app/oAuth.py
+++ app/oAuth.py
@@ -1,4 +1,7 @@
 from datetime import datetime, timedelta, timezone
+from fastapi import status, HTTPException
+from jwt.exceptions import InvalidTokenError
+from . import schema
 import jwt
 
 
@@ -19,6 +22,21 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+async def verify_access_token(token):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("userID")
+        if id is None:
+            raise credentials_exception
+        token_data = schema.TokenData(id=id)
+    except InvalidTokenError:
+        raise credentials_exception
+   
 
 
 
