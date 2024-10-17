ce4ec2a1af5b66fc4abf3ad3c8ad6a1f68436b3a
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-17 03:18:43 +0000

    chore/ writing the votes endpoint

--- app/routers/votes.py
+++ app/routers/votes.py
@@ -13,15 +13,24 @@ router = APIRouter(
 @router.post('/')
 def addVotes(votes = schema.VotesData, db:Session = Depends('get_db'), current_user = Depends(oAuth.getCurrentUser)):
     vote_query = db.query(models.Votes.postId).filter(models.Votes.postId == votes.postId, models.Votes.userId == current_user.userId)
-    vote_data = vote_query.first()
+    found_vote_data = vote_query.first()
     
     if(votes.action == 1):
-        raise HTTPException(status_code = status.HTTP_409_CONFLICT, details = "you have already voted. ypu cannot vote again!")
-    
-    new_vote = models.Votes(postId == votes.postId, userId == current_user.userId)
-    db.add()
-    db.commit()
-    return {"message": "Vote added successfully"}
+        if found_vote_data:
+            raise HTTPException(status_code = status.HTTP_409_CONFLICT, details = "you have already voted. ypu cannot vote again!")
+        new_vote = models.Votes(postId == votes.postId, userId == current_user.userId)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "Vote added successfully"}
     else:
-        if(votes.action == 0):
+        if not found_vote_data:
+            raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, details = "Cannot find vote!")
+        vote_query.delete(synchronize_session = False)
+        db.commit()
+                return {"message": "Vote deleted successfully"}
+
+        
+        
+
+            
             
